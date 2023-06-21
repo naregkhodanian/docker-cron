a77288475f3ab8b28157f7dc3eb428b035ca285e
Nareg Khodanian <nkhodanian@gmail.com>
2023-06-20 22:34:44 -0700

    Change internal domain

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ env >> /etc/environment
 # Run the job every 15 minutes using curl and log the response
 while true; do
     echo "Running the job..."
-    response=$(curl -s http://la-propane-app/quickbooks/run-job)
+    response=$(curl -s http://la-propane-portal/quickbooks/run-job)
     echo "Job response: $response"
     echo "Job completed."
     sleep 900  # Sleep for 900 seconds (15 minutes)
