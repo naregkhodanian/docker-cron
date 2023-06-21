dc51fa058110831cc7af67ecd9375b9405013446
Nareg Khodanian <nkhodanian@gmail.com>
2023-06-20 22:06:02 -0700

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,16 @@
 
 env >> /etc/environment
 
-# Run Laravel queue worker
-php artisan queue:work --daemon &
+# Set the working directory to the root directory of the Laravel app
+cd /app
+
+# Run Laravel queue worker with database driver
+php artisan --no-interaction queue:work database &
 
 # Run Laravel scheduler
 while [ true ]
 do
-    php artisan schedule:run --verbose --no-interaction
+    php artisan --no-interaction schedule:run
     sleep 60
 done
 
