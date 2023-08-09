92e1ce7e30e42dd0ab2655d4dc847258c6448455
Nareg Khodanian <nkhodanian@gmail.com>
2023-08-08 18:59:50 -0700

    new entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-# Set the bearer token from the environment variable
-BEARER_TOKEN="Bearer ${STATIC_API_TOKEN}"
-
 # Run the job every 15 minutes using curl and log the response
 while true; do
     echo "Running the job..."
-    response=$(curl -s -H "Authorization: ${BEARER_TOKEN}" https://app.la-propane.com/api/quickbooks/run-job)
+    response=$(curl -s -H "Authorization: Bearer ${AUTH_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job")
     echo "Job response: $response"
     echo "Job completed."
     sleep 43200  # Sleep for 12 hours
