c6392e5e967530a624956dcc60151843640d1a7f
Nareg Khodanian <nkhodanian@gmail.com>
2023-09-11 22:39:54 -0700

    Change env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Run the job every 15 minutes using curl and log the response
 while true; do
     echo "Running the job..."
-    response=$(curl -s -H "Authorization: Bearer ${AUTH_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job")
+    response=$(curl -s -H "Authorization: Bearer ${STATIC_API_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job")
     echo "Job response: $response"
     echo "Job completed."
     sleep 43200  # Sleep for 12 hours
@@ -12,3 +12,5 @@ done
 # Execute CMD
 echo "$@"
 exec "$@"
+
+curl -s -H "Authorization: Bearer ${STATIC_API_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job"
