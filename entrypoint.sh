abcf69203181bc878d165ad6fcd346af1d79f78d
naregkhodanian <nkhodanian@gmail.com>
2024-11-16 23:00:51 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-# Run the job every 15 minutes using curl and log the response
-while true; do
-    echo "Running the job..."
-    response=$(curl -s -H "Authorization: Bearer ${STATIC_API_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job")
-    echo "Job response: $response"
-    echo "Job completed."
-    sleep 43200  # Sleep for 12 hours
-done
+# Function to run the QuickBooks job every 12 hours
+run_quickbooks_job() {
+    while true; do
+        echo "Running the QuickBooks job..."
+        response=$(curl -s -H "Authorization: Bearer ${STATIC_API_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job")
+        echo "Job response: $response"
+        echo "QuickBooks job completed."
+        sleep 43200  # Sleep for 12 hours
+    done
+}
 
-# Execute CMD
-echo "$@"
+# Start the QuickBooks job in the background
+run_quickbooks_job &
+
+# Run Laravel's queue worker
+echo "Starting Laravel queue worker..."
+php artisan queue:work --sleep=3 --tries=3 --stop-when-empty
+
+# Keep the container running
 exec "$@"
-
-curl -s -H "Authorization: Bearer ${STATIC_API_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job"
