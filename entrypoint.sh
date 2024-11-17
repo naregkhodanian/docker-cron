3b6f1318de5e6c5edcd299a76ca074ce66bb189f
naregkhodanian <nkhodanian@gmail.com>
2024-11-16 23:13:41 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-# Function to run the QuickBooks job every 12 hours
-run_quickbooks_job() {
-    while true; do
-        echo "Running the QuickBooks job..."
-        response=$(curl -s -H "Authorization: Bearer ${STATIC_API_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job")
-        echo "Job response: $response"
-        echo "QuickBooks job completed."
-        sleep 43200  # Sleep for 12 hours
-    done
-}
+# Run the job every 15 minutes using curl and log the response
+while true; do
+    echo "Running the job..."
+    response=$(curl -s -H "Authorization: Bearer ${STATIC_API_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job")
+    echo "Job response: $response"
+    echo "Job completed."
+    sleep 43200  # Sleep for 12 hours
+done
 
-# Start the QuickBooks job in the background
-run_quickbooks_job 
+# Execute CMD
+echo "$@"
+exec "$@"
+
+curl -s -H "Authorization: Bearer ${STATIC_API_TOKEN}" -H "Accept: */*" -H "Accept-Encoding: gzip, deflate, br" -H "Connection: keep-alive" "http://la-propane-portal:8080/api/quickbooks/run-job"
